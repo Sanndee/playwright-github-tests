51f472aa9aea7d79979c361e242b38ae93f9df9d
91905 <you@example.com>
2026-06-25 06:45:16 +0530

    practice gitt3

--- tests/login_3.spec.ts
+++ tests/login_3.spec.ts
@@ -1,9 +1,10 @@
-import {test} from "@playwright/test"
+import {test, expect} from "@playwright/test"
 
 
 test(" devloping the login page wiith github 3",async({page})=>{
 
 
 await page.goto("https://www.amazon.in/");
+await expect(page).toHaveTitle("amazon");
 
 })
