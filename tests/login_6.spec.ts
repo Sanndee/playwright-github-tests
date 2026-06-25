4b37718c377ed6feb4fedae01a45c5e84f17f888
91905 <you@example.com>
2026-06-25 07:06:41 +0530

    practice_6th_comment

---
+++ tests/login_6.spec.ts
@@ -0,0 +1,10 @@
+import {test, expect} from "@playwright/test"
+
+
+test(" devloping the login page wiith github 3",async({page})=>{
+
+
+await page.goto("https://www.amazon.in/");
+await expect(page).toHaveTitle("amazon");
+await page.getByPlaceholder("Search Amazon.in").fill("samsung")
+})
